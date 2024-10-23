9d34432f0ae89c138d31615499d508f55b839981
Bruno <bruno.rp45@gmail.com>
2024-10-23 11:55:19 -0300

    Adicionando @angular/fire

--- emoTrack/src/app/app.module.ts
+++ emoTrack/src/app/app.module.ts
@@ -6,12 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { environment } from '../environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,10 +18,8 @@ import { environment } from '../environments/environment';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig), 
-    AngularFireAuthModule 
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ "projectId": "emotrack-d29ec", "appId": "1:6277795934:web:5e45bbf56ab15fda9ce4cf", "storageBucket": "emotrack-d29ec.appspot.com", "apiKey": "AIzaSyALz2xLpdWrm6-yVJwMST8bQ3LSzrrnjiI", "authDomain": "emotrack-d29ec.firebaseapp.com", "messagingSenderId": "6277795934" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- emoTrack/src/app/services/auth-service.service.ts
+++ emoTrack/src/app/services/auth-service.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';  //aqui
-import { Router } from '@angular/router';  //aqui
-import { Auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import { Router } from '@angular/router'; 
+import { Auth, signInWithEmailAndPassword, signInWithPopup, createUserWithEmailAndPassword, GoogleAuthProvider } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private router: Router) { }
+  constructor(
+    private auth: Auth, 
+    private router: Router) { }
 
   async register(email: string, password: string){
-    await this.auth.createUserWithEmailAndPassword(email, password);
+    await createUserWithEmailAndPassword(this.auth, email, password);
   }
 
   async login(email: string, password: string) {
     try {
-      await this.auth.signInWithEmailAndPassword(email, password)
+      await signInWithEmailAndPassword(this.auth, email, password)
       this.router.navigate(['home']);
     } catch (error) {
       this.router.navigate(['login-erro']);
@@ -29,6 +30,6 @@ export class AuthService {
   // Login com Google
   async loginWithGoogle() {
     const provider = new GoogleAuthProvider();
-    return this.auth.signInWithPopup(provider);
+    return signInWithPopup(this.auth, provider);
   }
 }
